c14c756757eed504513f2f036f777c3994b05f7c
Stan Sobolev <iamjacke@gmail.com>
2024-06-23 10:32:01 +0300

    feat(log)

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,10 @@ use tokio::signal;
 use dptree::di::DependencyMap;
 use reqwest::ClientBuilder;
 use tokio::time::sleep;
+use log::{error, warn, info, debug, trace};
+use pretty_env_logger;
+use simplelog::*;
+use std::fs::File;
 
 mod commands;
 mod db;
@@ -35,22 +39,19 @@ enum Command {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    pretty_env_logger::init();
     // Initialize simplelog for file logging
     CombinedLogger::init(vec![
-        TermLogger::new(
-            LevelFilter::Error,
-            Config::default(),
-            TerminalMode::Mixed,
-            ColorChoice::Auto,
-        ),
         WriteLogger::new(
             LevelFilter::Error,
             Config::default(),
             File::create("app.log").unwrap(),
         ),
     ])
-    .unwrap();    
+    .unwrap();
+
+    // Initialize pretty_env_logger for console logging
+    pretty_env_logger::init();
+    
     log::info!("Starting bot...");
 
     let bot = Bot::from_env_with_client(
