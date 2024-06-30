696521dff7d1e3b8bea71a793e463a60c33aedc4
Stan Sobolev <iamjacke@gmail.com>
2024-06-30 10:49:45 +0300

    chore(cleanup)

---
+++ src/downloader.rs
@@ -0,0 +1,148 @@
+use teloxide::prelude::*;
+use teloxide::types::InputFile;
+use crate::rate_limiter::RateLimiter;
+use crate::fetch::fetch_song_metadata;
+use crate::utils::escape_filename;
+use std::sync::Arc;
+use std::time::Duration;
+use url::Url;
+use thiserror::Error;
+use anyhow::{Error, anyhow};
+use std::process::Command;
+
+#[derive(Error, Debug)]
+pub enum CommandError {
+    #[error("Failed to fetch song metadata")]
+    FetchMetadata(#[from] Error),
+    #[error("Failed to download file")]
+    Download(Error),
+}
+
+pub async fn download_and_send_audio(bot: Bot, msg: Message, url: Url, rate_limiter: Arc<RateLimiter>) -> ResponseResult<()> {
+    let bot_clone = bot.clone();
+    let chat_id = msg.chat.id;
+    let _rate_limiter = Arc::clone(&rate_limiter);
+
+    tokio::spawn(async move {
+        let result: Result<(), CommandError> = async {
+            let (title, artist) = fetch_song_metadata(&url.as_str())
+                .await
+                .map_err(|e| CommandError::FetchMetadata(anyhow!("Failed to fetch song metadata: {}", e)))?;
+            let file_name = generate_file_name(&title, &artist);
+            let safe_filename = escape_filename(&file_name);
+            let full_path = format!("~/downloads/{}", safe_filename);
+            let download_path = shellexpand::tilde(&full_path).into_owned();
+            
+            let mut child = Command::new("youtube-dl")
+                .arg("-o")
+                .arg(&download_path)
+                .arg("--verbose")
+                .arg("--extract-audio")
+                .arg("--audio-format")
+                .arg("mp3")
+                .arg("--audio-quality")
+                .arg("0")
+                .arg("--add-metadata")
+                .arg("--embed-thumbnail")
+                .arg("--no-playlist")
+                .arg("--postprocessor-args")
+                .arg("-acodec libmp3lame -b:a 320k") // Set audio codec to libmp3lame and bitrate to 320 kbps 
+                .arg(url.as_str())
+                .spawn()
+                .expect("Failed to start youtube-dl process");
+            let _ = child.wait().expect("youtube-dl process failed");
+
+            println!("download_path {:?}", download_path);
+
+            // Use ffprobe to get the duration of the audio file
+            let output = Command::new("ffprobe")
+                .args(&[
+                    "-v", "error",
+                    "-show_entries", "format=duration",
+                    "-of", "default=noprint_wrappers=1:nokey=1",
+                    &download_path,
+                ])
+                .output()
+                .expect("Failed to execute ffprobe");
+            let duration_str = String::from_utf8_lossy(&output.stdout).trim().to_string();
+            let duration: u32 = duration_str.parse::<f32>().unwrap_or(0.0).round() as u32;
+
+            bot_clone
+                .send_audio(chat_id, InputFile::file(&download_path))
+                .duration(duration)
+                .await
+                .map_err(|e| CommandError::Download(anyhow!("Failed to send audio file: {}", e)))?;
+                
+            tokio::time::sleep(Duration::from_secs(600)).await;
+            std::fs::remove_file(&download_path).expect("Failed to delete file");
+
+            Ok(())
+        }.await;
+
+        if let Err(e) = result {
+            bot_clone
+                .send_message(chat_id, format!("An error occurred: {}", e))
+                .await
+                .unwrap();
+        }
+    });
+    Ok(())
+}
+
+pub async fn download_and_send_video(bot: Bot, msg: Message, url: Url, rate_limiter: Arc<RateLimiter>) -> ResponseResult<()> {
+    let bot_clone = bot.clone();
+    let chat_id = msg.chat.id;
+    let _rate_limiter = Arc::clone(&rate_limiter);
+
+    tokio::spawn(async move {
+        let result: Result<(), CommandError> = async {
+            let file_name = "video.mp4"; // You can generate a better name based on metadata if needed
+            let safe_filename = escape_filename(&file_name);
+            let full_path = format!("~/downloads/{}", safe_filename);
+            let download_path = shellexpand::tilde(&full_path).into_owned();
+            
+            let mut child = Command::new("youtube-dl")
+                .arg("-o")
+                .arg(&download_path)
+                .arg("--verbose")
+                .arg("--format")
+                .arg("best")
+                .arg(url.as_str())
+                .spawn()
+                .expect("Failed to start youtube-dl process");
+            let _ = child.wait().expect("youtube-dl process failed");
+
+            println!("download_path {:?}", download_path);
+
+            bot_clone
+                .send_video(chat_id, InputFile::file(&download_path))
+                .await
+                .map_err(|e| CommandError::Download(anyhow!("Failed to send video file: {}", e)))?;
+                
+            tokio::time::sleep(Duration::from_secs(600)).await;
+            std::fs::remove_file(&download_path).expect("Failed to delete file");
+
+            Ok(())
+        }.await;
+
+        if let Err(e) = result {
+            bot_clone
+                .send_message(chat_id, format!("An error occurred: {}", e))
+                .await
+                .unwrap();
+        }
+    });
+    Ok(())
+}
+
+fn generate_file_name(title: &str, artist: &str) -> String {
+    if artist.trim().is_empty() && title.trim().is_empty() {
+        "Unknown.mp3".to_string()
+    } else if artist.trim().is_empty() {
+        format!("{}.mp3", title)
+    } else if title.trim().is_empty() {
+        format!("{}.mp3", artist)
+    } else {
+        format!("{} - {}.mp3", artist, title)
+    }
+}
